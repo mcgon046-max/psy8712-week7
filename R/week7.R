9d8c06f9dbdecbd7b4bd23a49a8fc25d68aa33af
Patrick Mcgonagle <mcgon046@umn.edu>
2026-03-15 23:18:01 -0500

    data is clean

--- R/week7.R
+++ R/week7.R
@@ -9,6 +9,9 @@ library(tidyverse)
 week7_tbl <- read_csv("../data/week3.csv") |>
   mutate(timeStart = ymd_hms(timeStart), 
          timeEnd = ymd_hms(timeEnd),
-         condition = fct_recode(
-           
-         ))
+         condition = fct_recode(condition,"Block A" = "A", "Block B" = "B","Control" = "C"),
+         gender = fct_recode(gender, "Male" = "M", "Female" = "F")) |>
+  filter(q6 == 1) |>
+  select(-q6) |>
+  mutate(timeSpent = difftime(timeEnd, timeStart, units = "mins")) # Note I did not capitalize condition and gender, which may be a mistake as this is different from table 1, but I wasn't sure. 
+         

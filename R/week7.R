230762a91f534b8a4886f3e584046f225a2f2c00
Patrick Mcgonagle <mcgon046@umn.edu>
2026-03-16 00:16:17 -0500

    Fig 3 complete

--- R/week7.R
+++ R/week7.R
@@ -27,3 +27,15 @@ fig1 <- week7_tbl |>
         geom_point() +
         labs(x = "Date of Experiment", y = "Q1 Score")
 ggsave(filename = "../figs/fig1.png", plot = fig1, width = 10, height = 10, dpi = 300)
+
+fig2 <- week7_tbl |>
+        ggplot(aes(x = q1, y = q2, color = gender)) +
+        geom_jitter(width = 0.2, height = 0.2, alpha = 1) +
+  scale_color_manual(name = "Participant Gender", values = c("Female" = "#00BFC4", "Male" = "#F8766D"), breaks = c("Male", "Female"))
+ggsave(filename = "../figs/fig2.png", plot = fig2, width = 10, height = 10, dpi = 300)
+fig3 <- week7_tbl |>
+        ggplot(aes(x = q1, y = q2)) +
+        geom_jitter(width = 0.2, height = 0.2, alpha = 1) +
+        facet_grid(cols = vars(factor(gender, levels = c("Male", "Female")))) +
+        labs(x = "Score on Q1", y = "Score on Q2") # Had to do a factor call in the middle of the facet_grid layer to get the order correct, I hope this doesn't mean I messed something up above...
+ggsave(filename = "../figs/fig3.png", plot = fig3, width = 10, height = 10, dpi = 300)

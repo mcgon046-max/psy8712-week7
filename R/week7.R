9657f7090fd453dcfd7919153e5fc81fc476e5ea
Patrick Mcgonagle <mcgon046@umn.edu>
2026-03-15 23:39:53 -0500

    Save

--- R/week7.R
+++ R/week7.R
@@ -1,7 +1,7 @@
 # Script Settings and Resources 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
-
+library(GGally)
 
 
 
@@ -13,5 +13,11 @@ week7_tbl <- read_csv("../data/week3.csv") |>
          gender = fct_recode(gender, "Male" = "M", "Female" = "F")) |>
   filter(q6 == 1) |>
   select(-q6) |>
-  mutate(timeSpent = difftime(timeEnd, timeStart, units = "mins")) # Note I did not capitalize condition and gender, which may be a mistake as this is different from table 1, but I wasn't sure. 
-         
+  mutate(timeSpent = difftime(timeEnd, timeStart, units = "mins")) # Note I did not capitalize condition and gender, which may be a mistake as this is different from table 1, but I wasn't sure.
+
+
+
+
+# Visualization
+ggpairs(subset(week7_tbl, select = paste0("q", c(1:5, 7:10))))
+ggsave(filename = "../figs/fig0.png", width = 10, height = 10, dpi = 300)
